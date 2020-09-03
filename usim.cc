a4c6e6e2526aa37af2afc930c1babb5533383a95
NuclearManD <dylanbrophy@gmail.com>
2020-09-03 08:22:43 -0700

    don't use c/c++ io!

--- usim.cc
+++ usim.cc
@@ -7,7 +7,6 @@
 //
 
 #include <cstdlib>
-#include <cstdio>
 #include "usim.h"
 
 //----------------------------------------------------------------------------
@@ -49,14 +48,6 @@ Word USim::fetch_word(void)
 	return val;
 }
 
-void USim::invalid(const char *msg)
-{
-	fprintf(stderr, "\r\ninvalid %s : pc = [%04x], ir = [%04x]\r\n",
-		msg ? msg : "",
-		pc, ir);
-	halt();
-}
-
 //----------------------------------------------------------------------------
 // Primitive (byte) memory access routines
 //----------------------------------------------------------------------------
@@ -71,68 +62,6 @@ Byte USim::read(Word offset)
 void USim::write(Word offset, Byte val)
 {
 	memory[offset] = val;
-}
-
-//----------------------------------------------------------------------------
-// Processor loading routines
-//----------------------------------------------------------------------------
-static Byte fread_byte(FILE *fp)
-{
-	char			str[3];
-	long			l;
-
-	str[0] = fgetc(fp);
-	str[1] = fgetc(fp);
-	str[2] = '\0';
-
-	l = strtol(str, NULL, 16);
-	return (Byte)(l & 0xff);
-}
-
-static Word fread_word(FILE *fp)
-{
-	Word		ret;
-
-	ret = fread_byte(fp);
-	ret <<= 8;
-	ret |= fread_byte(fp);
-
-	return ret;
-}
-
-void USim::load_intelhex(const char *filename)
-{
-	FILE		*fp;
-	int		done = 0;
-
-	fp = fopen(filename, "r");
-	if (!fp) {
-		perror("filename");
-		exit(EXIT_FAILURE);
-	}
-
-	while (!done) {
-		Byte		n, t;
-		Word		addr;
-		Byte		b;
-
-		(void)fgetc(fp);
-		n = fread_byte(fp);
-		addr = fread_word(fp);
-		t = fread_byte(fp);
-		if (t == 0x00) {
-			while (n--) {
-				b = fread_byte(fp);
-				memory[addr++] = b;
-			}
-		} else if (t == 0x01) {
-			pc = addr;
-			done = 1;
-		}
-		// Read and discard checksum byte
-		(void)fread_byte(fp);
-		if (fgetc(fp) == '\r') (void)fgetc(fp);
-	}
 }
 
 //----------------------------------------------------------------------------
